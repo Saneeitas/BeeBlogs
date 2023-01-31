8d493d68698a19c9422919404a10f83e7c84c816
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2023-01-31 02:27:45 +0100

    first commit

---
+++ inc/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$database = "content_management_system";
+$username = "root";
+$password = "";
+//connecting to the database
+$connection = mysqli_connect($host,$username,$password,$database)
+or die("Database cannot connect");
+
+ 
